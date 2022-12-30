fc39b7dc2576e02ab2b4941815208b5482809c22
marcelino pangilinan <marbysneaker@gmail.com>
2022-12-29 22:15:28 -0800

    added CSS

--- src/components/card-list/card-list.component.jsx
+++ src/components/card-list/card-list.component.jsx
@@ -1,13 +1,22 @@
 import { Component } from "react";
+import './card-list.style.css'
 
 class CardList extends Component {
    render(){ 
-    const {monsters} =this.props
+    const {monsters} =this.props;
+        
         return(
-             <div>
-              {monsters.map((monster)=> (
-                <h1 key = {monster.id}>{monster.name}</h1>
-              ))}
+             <div className="card-list">
+              {monsters.map((monster)=> {
+                const {name, email, id} =monster;
+                return(
+                <div className="card-container" key={id}>
+                    <img alt={`monster ${name}`}  src={`https://robohash.org/${id}?set=2&size180x180`}/>
+                    <h2>{name}</h2>
+                    <p>{email}</p>
+                </div> 
+                )
+                })}
             </div>        
         );
     }
